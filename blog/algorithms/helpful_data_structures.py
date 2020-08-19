a561e8e365c14ae0b2829421fe49ba905c76589b
Nick Fausti <ncfausti@gmail.com>
2020-08-19 15:57:37 -0400

    added heapq merge based find median of two sorted lists

--- blog/algorithms/helpful_data_structures.py
+++ blog/algorithms/helpful_data_structures.py
@@ -2,6 +2,23 @@ import heapq
 from collections import deque
 from itertools import product, combinations, combinations_with_replacement, permutations
 
+# Use heapq to efficientl keep track of updated values from list
+from heapq import merge
+
+
+def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+    results = list(merge(nums1, nums2))
+    N = len(results)
+    if N % 2 == 0:
+        mid = (N // 2) - 1 # 12 -> 6-1 = 5
+        # mid + 2 since python list slice 0:2 returns 0, 1 elements
+        return sum(results[mid:mid+2]) / 2 
+    return results[N//2] # e.g. 5 -> 2 0,1, 2 ,3 4
+        
+        
+
+
+
 # product(ls, repeat=2)
 # https://docs.python.org/3.8/library/itertools.html
 
